6ddf24f51f4c39d7d85d791e2ccc9d5aad12d60a
Cory Loeffelman <Cloeffelman@shelterinsurance.com>
2018-04-13 13:55:38 -0500

    Initial commit.

---
+++ src/components/app/appWrapper.jsx
@@ -0,0 +1,22 @@
+import {connect} from 'react-redux'
+
+import App from './app'
+
+const mapStateToProps = state => {
+  return {
+    
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    
+  }
+}
+
+const AppWrapper = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App)
+
+export default AppWrapper
